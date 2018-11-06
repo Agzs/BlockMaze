167bcb332241a8cc426a6a6df70cf6322ed7ace6
Agzs <gzs_1994@163.com>
2018-11-06 15:12:29 +0800

    test comparison gadget for 64-bits.

--- libsnark-hdsnark/src/comparison/cmp_test.cpp
+++ libsnark-hdsnark/src/comparison/cmp_test.cpp
@@ -10,7 +10,7 @@
 
 #include "libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp"
 #include "libsnark/common/default_types/r1cs_ppzksnark_pp.hpp"
-//#include "libsnark/common/utils.hpp"
+// #include "libsnark/common/utils.hpp"
 #include <boost/optional.hpp>
 
 
@@ -29,48 +29,8 @@ using namespace std;
  * (3) The "verifier", which runs the ppzkSNARK verifier on input the verification key,
  *     a primary input for CS, and a proof.
  */
-//template<typename ppzksnark_ppT>
-//bool run_r1cs_gg_ppzksnark(const r1cs_example<Fr<ppzksnark_ppT> > &example)
-//{
-//    libff::print_header("R1CS GG-ppzkSNARK Generator");
-//    r1cs_gg_ppzksnark_keypair<ppzksnark_ppT> keypair = r1cs_gg_ppzksnark_generator<ppzksnark_ppT>(example.constraint_system);
-//    printf("\n"); libff::print_indent(); libff::print_mem("after generator");
-//
-//    libff::print_header("Preprocess verification key");
-//    r1cs_gg_ppzksnark_processed_verification_key<ppzksnark_ppT> pvk = r1cs_gg_ppzksnark_verifier_process_vk<ppzksnark_ppT>(keypair.vk);
-//
-//    libff::print_header("R1CS GG-ppzkSNARK Prover");
-//    r1cs_gg_ppzksnark_proof<ppzksnark_ppT> proof = r1cs_gg_ppzksnark_prover<ppzksnark_ppT>(keypair.pk, example.primary_input, example.auxiliary_input);
-//    printf("\n"); libff::print_indent(); libff::print_mem("after prover");
-//
-//    libff::print_header("R1CS GG-ppzkSNARK Verifier");
-//    const bool ans = r1cs_gg_ppzksnark_verifier_strong_IC<ppzksnark_ppT>(keypair.vk, example.primary_input, proof);
-//    printf("\n"); libff::print_indent(); libff::print_mem("after verifier");
-//    printf("* The verification result is: %s\n", (ans ? "PASS" : "FAIL"));
-//
-//    libff::print_header("R1CS GG-ppzkSNARK Online Verifier");
-//    const bool ans2 = r1cs_gg_ppzksnark_online_verifier_strong_IC<ppzksnark_ppT>(pvk, example.primary_input, proof);
-//    assert(ans == ans2);
-//
-//    return ans;
-//}
 
-//template<typename ppzksnark_ppT>
-//void test_r1cs_gg_ppzksnark(size_t num_constraints, size_t input_size)
-//{
-//    r1cs_example<Fr<ppzksnark_ppT> > example = generate_r1cs_example_with_binary_input<Fr<ppzksnark_ppT> >(num_constraints, input_size);
-//    const bool bit = run_r1cs_gg_ppzksnark<ppzksnark_ppT>(example);
-//    assert(bit);
-//}
-
-// int main () {
-//     default_r1cs_gg_ppzksnark_pp::init_public_params();
-//     test_r1cs_gg_ppzksnark<default_r1cs_gg_ppzksnark_pp>(1000, 100);
-
-//     return 0;
-// }
-#define DEBUG 1
-
+#define DEBUG 0
 
 // 生成proof
 template<typename ppzksnark_ppT>
@@ -112,10 +72,6 @@ void PrintProof(r1cs_ppzksnark_proof<ppzksnark_ppT> proof)
     //printf("proof is %x\n", *proof);
     std::cout << "comparison proof:\n";
 
-    // std::cout << "\n knowledge_commitment<G2<ppT>, G1<ppT> > g_B: \n " << proof.g_B << endl;
-    // std::cout << "\n knowledge_commitment<G2<ppT>, G1<ppT> > g_B: \n " << proof.g_B << endl;
-    // std::cout << "\n knowledge_commitment<G1<ppT>, G1<ppT> > g_C: \n " << proof.g_C << endl;
-
     std::cout << "\n knowledge_commitment<G1<ppT>, G1<ppT> > g_A: ";
     std::cout << "\n   knowledge_commitment.g: \n     " << proof.g_A.g;
     std::cout << "\n   knowledge_commitment.h: \n     " << proof.g_A.h << endl;
@@ -136,8 +92,10 @@ void PrintProof(r1cs_ppzksnark_proof<ppzksnark_ppT> proof)
 
 // test_comparison_gadget_with_instance
 template<typename ppzksnark_ppT> //--Agzs
-bool test_comparison_gadget_with_instance(const size_t n, const size_t a, const size_t b)
+bool test_comparison_gadget_with_instance(const size_t a, const size_t b)
 {
+   const size_t n = 63; // 由于 1ul 表示无符号长整形(64bit), 最高位不作符号位，所以n最大取63
+
     printf("testing comparison_gadget on all %zu bit inputs: a = %zu, b = %zu\n", n, a, b);
 
     typedef libff::Fr<ppzksnark_ppT> FieldT;
@@ -159,7 +117,12 @@ bool test_comparison_gadget_with_instance(const size_t n, const size_t a, const 
     // key pair generation
     r1cs_ppzksnark_keypair<ppzksnark_ppT> keypair =  r1cs_ppzksnark_generator<ppzksnark_ppT>(constraint_system);
 
-    if (a < 1ul<<n && b < 1ul<<n)
+    /* ****************************************************************
+     * ul 表示无符号长整形, 最高位不作符号位
+     * 1ul<<63 = 1000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 = 9223372036854775808
+     * 64个1 = 18446744073709551615
+     * *****************************************************************/
+    if (a < 1ul<<n && b < 1ul<<n) // 
     {
         pb.val(A) = FieldT(a);
         pb.val(B) = FieldT(b);
@@ -173,6 +136,9 @@ bool test_comparison_gadget_with_instance(const size_t n, const size_t a, const 
         assert(pb.val(less_or_eq) == (a <= b ? FieldT::one() : FieldT::zero()));
         assert(pb.is_satisfied());
 
+	    // generate proof
+        auto proof = generate_proof(keypair.pk, pb);
+
         if (pb.val(less_or_eq) == FieldT::one())
         {
             if (pb.val(less) == FieldT::one()) {
@@ -184,9 +150,6 @@ bool test_comparison_gadget_with_instance(const size_t n, const size_t a, const 
             printf("result test for %zu > %zu\n", a, b);
         }
 
-	    // generate proof
-        auto proof = generate_proof(keypair.pk, pb);
-
         // verify proof
         if (!proof) {
             return false;
@@ -195,7 +158,7 @@ bool test_comparison_gadget_with_instance(const size_t n, const size_t a, const 
             // std::cout<<"NULL input: "<<input<<endl;
             PrintProof(*proof);
 
-            return verify_proof(keypair.vk, *proof);
+            assert(verify_proof(keypair.vk, *proof));
         }
 
     } else {
@@ -203,55 +166,11 @@ bool test_comparison_gadget_with_instance(const size_t n, const size_t a, const 
         return false;
     }
 
-    libff::print_time("comparison tests successful");
+    printf("the %zu bit inputs, comparison tests successful", n);
+    // libff::print_time("the %zu bit inputs, comparison tests successful", n);
     printf("\n");
     return true;
 }
-
-template<typename ppzksnark_ppT>
-void my_test_comparison_gadget(const size_t n)
-{
-    printf("testing comparison_gadget on all %zu bit inputs\n", n);
-    typedef libff::Fr<ppzksnark_ppT> FieldT;
-
-    protoboard<FieldT> pb;
-
-    pb_variable<FieldT> A, B, less, less_or_eq;
-    A.allocate(pb, "A");
-    B.allocate(pb, "B");
-    less.allocate(pb, "less");
-    less_or_eq.allocate(pb, "less_or_eq");
-
-    comparison_gadget<FieldT> cmp(pb, n, A, B, less, less_or_eq, "cmp");
-    cmp.generate_r1cs_constraints();
-
-    // Check constraints of the R1CS system
-//    const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
-//    std::cout << "Number of R1CS constraints: " << constraint_system.num_constraints() << endl;
-//
-//    r1cs_ppzksnark_generator<ppzksnark_ppT> keypair =  r1cs_ppzksnark_generator<ppzksnark_ppT>(constraint_system);
-
-    for (size_t a = 0; a < 1ul<<n; ++a)
-    {
-        for (size_t b = 0; b < 1ul<<n; ++b)
-        {
-            pb.val(A) = FieldT(a);
-            pb.val(B) = FieldT(b);
-
-            cmp.generate_r1cs_witness();
-
-#ifdef DEBUG
-            printf("positive test for %zu < %zu\n", a, b);
-#endif
-            assert(pb.val(less) == (a < b ? FieldT::one() : FieldT::zero()));
-            assert(pb.val(less_or_eq) == (a <= b ? FieldT::one() : FieldT::zero()));
-            assert(pb.is_satisfied());
-        }
-    }
-
-    libff::print_time("comparison tests successful");
-}
-
 
 int main () {
     default_r1cs_gg_ppzksnark_pp::init_public_params();
@@ -259,7 +178,10 @@ int main () {
 
     libff::print_header("#             test comparison gadget with assert()");
 
-    test_comparison_gadget_with_instance<default_r1cs_gg_ppzksnark_pp>(6, 45, 40);
+    // test_comparison_gadget_with_instance<default_r1cs_gg_ppzksnark_pp>(45, 40);
+
+    test_comparison_gadget_with_instance<default_r1cs_gg_ppzksnark_pp>(40, 9223372036854775807);
+
     // assert(test_comparison_gadget_with_instance<default_r1cs_gg_ppzksnark_pp>(6, 45, 40)); 
     // Note. cmake can not compile the assert()  --Agzs
     
