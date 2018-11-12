b46411209484072c476dc37d2eaf03e543fc0167
Agzs <gzs_1994@163.com>
2018-11-12 14:29:37 +0800

    update subtractionLess with inheriting packing_gadget

--- libsnark-hdsnark/src/subgadget/subtractionLess/main.cpp
+++ libsnark-hdsnark/src/subgadget/subtractionLess/main.cpp
@@ -306,31 +306,31 @@ public:
 
 
 template<typename FieldT>
-class note_gadget_with_comparison_and_subtraction_for_value_old : public gadget<FieldT> { // 基类和比较类组合，基本的note_gadget和comparison_gadget (value_s)
+class note_gadget_with_comparison_and_subtraction_for_value_old : public note_gadget_with_packing<FieldT> { // 基类和比较类组合，基本的note_gadget和comparison_gadget (value_s)
 public:
-    std::shared_ptr<note_gadget_with_packing<FieldT>> packThree;
+    //std::shared_ptr<note_gadget_with_packing<FieldT>> packThree;
 
     std::shared_ptr<less_comparison_gadget<FieldT> > less_cmp;
 
-    note_gadget_with_comparison_and_subtraction_for_value_old(protoboard<FieldT> &pb) : gadget<FieldT>(pb) {
-        packThree.reset(new note_gadget_with_packing<FieldT>(pb));
+    note_gadget_with_comparison_and_subtraction_for_value_old(protoboard<FieldT> &pb) : note_gadget_with_packing<FieldT>(pb) {
+        //packThree.reset(new note_gadget_with_packing<FieldT>(pb));
 
-        less_cmp.reset(new less_comparison_gadget<FieldT>(pb, packThree->value_s_packed, packThree->value_old_packed,
+        less_cmp.reset(new less_comparison_gadget<FieldT>(pb, this->value_s_packed, this->value_old_packed,
                                                     FMT(this->annotation_prefix, " less_cmp")));
     }
 
     void generate_r1cs_constraints() { // const Note& note
-        packThree->generate_r1cs_constraints();
+        note_gadget_with_packing<FieldT>::generate_r1cs_constraints();
 
         // 1 * (value_old - value_s) = this->value 
-        this->pb.add_r1cs_constraint(r1cs_constraint<FieldT>(1, (packThree->value_old_packed - packThree->value_s_packed), packThree->value_packed),
+        this->pb.add_r1cs_constraint(r1cs_constraint<FieldT>(1, (this->value_old_packed - this->value_s_packed), this->value_packed),
                                  FMT(this->annotation_prefix, " equal"));
 
         less_cmp->generate_r1cs_constraints();
     }
 
     void generate_r1cs_witness(uint64_t v, uint64_t v_old, uint64_t v_s) { // 为变量生成约束
-        packThree->generate_r1cs_witness(v, v_old, v_s);
+        note_gadget_with_packing<FieldT>::generate_r1cs_witness(v, v_old, v_s);
 
         less_cmp->generate_r1cs_witness();
     }
