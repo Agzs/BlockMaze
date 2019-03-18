eef57d1e9ee60ef6bccfd55378e28550c23706d9
FZQA <913229100@qq.com>
2019-03-18 09:01:10 +0800

    change cgo-send

--- libsnark-vnt/src/send/sendcgo.hpp
+++ libsnark-vnt/src/send/sendcgo.hpp
@@ -10,16 +10,20 @@ extern "C"
     char *genCMTS(uint64_t value_s, char *pk_string, char *sn_s_string, char *r_s_string, char *sn_old_string);
 
     char *genSendproof(uint64_t value_A,
-                       char *sn_s_string,
-                       char *r_s_string,
-                       char *sn_string,
-                       char *r_string,
-                       char *cmt_s_string,
-                       char *cmtA_string,
-                       uint64_t value_s,
-                       char *pk_string);
+                   char *sn_s_string,
+                   char *r_s_string,
+                   char *sn_string,
+                   char *r_string,
+                   char *cmt_s_string,
+                   char *cmtA_string,
+                   uint64_t value_s,
+                   char *pk_string,
+                   uint64_t value_A_new,
+                   char *sn_A_new,
+                   char *r_A_new,
+                   char *cmt_A_new);
 
-    bool verifySendproof(char *data, char *sn_old_string, char *cmtS_string);
+    bool verifySendproof(char *data, char *cmtA_old_string, char *sn_old_string, char *cmtS_string ,char *cmtA_new_string);
 
 #ifdef __cplusplus
 } // extern "C"
