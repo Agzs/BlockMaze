95c309f661ec247ff5c6434a21e2d0be3aab5366
Agzs <gzs_1994@163.com>
2019-12-15 13:23:50 +0800

    fixed dashboard security alerts

--- go-ethereum/cmd/utils/flags.go
+++ go-ethereum/cmd/utils/flags.go
@@ -1185,7 +1185,7 @@ func RegisterEthService(stack *node.Node, cfg *eth.Config) {
 // RegisterDashboardService adds a dashboard to the stack.
 func RegisterDashboardService(stack *node.Node, cfg *dashboard.Config, commit string) {
 	stack.Register(func(ctx *node.ServiceContext) (node.Service, error) {
-		return dashboard.New(cfg, commit, ctx.ResolvePath("logs")), nil
+		return dashboard.New(cfg, commit)
 	})
 }
 
